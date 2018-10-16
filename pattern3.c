208829530a8530c3f86b8e586be184598bfcce21
Kalaiselvi. C <42495619+Kalaiselvi-C@users.noreply.github.com>
2018-10-16 21:35:20 +0530

    Add files via upload

---
+++ pattern3.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+
+{
+    int arr[100],i,j,n;
+    printf("Enter the value:");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        for(j=1;j<=n;j++)
+        {
+            printf("%d\t",j);
+        }
+        printf("\n");
+    }
+}
+
